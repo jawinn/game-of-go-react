0841f2039695734aa376ff6713718080ca5dd216
Josh Winn <jw@joshuawinn.com>
2022-05-08 12:37:50 -0400

    Responsive Board
    Create Board that is responsive relative to the viewport width and height. With a minimum size of the board relative to the tap target sizes of each square.

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,20 +6,24 @@ interface BoardProps {
   boardSize: number
 }
 
+const getBoardClass = (gridSize: number) => {
+  return styles[`board-${gridSize.toString()}x${gridSize.toString()}`];
+};
+
 /**
  * The square game board, made up of intersecting Points.
  * The grid can be different sizes: 19x19, 13x13, and 9x9 are standard.
  */
 const Board = (props: BoardProps) => {
-  const [boardSize, setBoardSize] = useState(9);
-  const [boardData, setBoardData] = useState(Array(boardSize * boardSize).fill(0));
+  const [boardSize] = useState(9);
+  const [boardData] = useState(Array(boardSize * boardSize).fill(0));
 
   const renderPoints = boardData.map((point, index) => {
     return <Point playerStone={0} boardSize={boardSize} key={index} />
   });
 
   return (
-    <ol className={styles.board} data-testid="Board">
+    <ol className={getBoardClass(boardSize)} data-testid="Board">
       {renderPoints}
     </ol>
   );
