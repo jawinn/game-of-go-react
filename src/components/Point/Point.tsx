27570ae64d38658a56fb60a0e0dd5739ae437da1
Josh Winn <jw@joshuawinn.com>
2022-05-08 21:29:04 -0400

    Add star points (dots) to board
    Add the star points to the board grid with a Point class, based on data for each board size. Location of points is slightly different per board size.

--- src/components/Point/Point.tsx
+++ src/components/Point/Point.tsx
@@ -9,6 +9,29 @@ interface PointProps {
   gridY: number
 }
 
+interface StarPoints {
+  boardSize: number,
+  stars: number[][]
+}
+
+/**
+ * Location of star points (dots), based on game-based grid starting value of 1.
+ */
+const boardStarPoints: StarPoints[] = [
+  {
+    boardSize: 9,
+    stars: [[3,3], [3,7], [5,5], [7,7], [7,3]]
+  },
+  {
+    boardSize: 13,
+    stars: [[4,4], [4,7], [4,10], [7,4], [7,7], [7,10], [10,4], [10,7], [10,10]]
+  },
+  {
+    boardSize: 19,
+    stars: [[4,4], [4,10], [4,16], [10,4], [10,10], [10,16], [16,4], [16,10], [16,16]]
+  },
+];
+
 /**
  * Get class names for the point element; special classes are used for points 
  * on the edges of the board, and for star/dot markers.
@@ -18,16 +41,25 @@ const getPointClass = (boardSize: number, gridX: number, gridY: number) => {
 
   if (gridX === 0){
     classNames.push(styles.top);
-  } else if (gridX === boardSize - 1) {
+  } else if (gridX === boardSize - 1){
     classNames.push(styles.bottom);
   }
 
   if (gridY === 0){
     classNames.push(styles.left);
-  } else if (gridY === boardSize - 1) {
+  } else if (gridY === boardSize - 1){
     classNames.push(styles.right);
   }
 
+  let starPoints: StarPoints | undefined = boardStarPoints.find(point => point.boardSize === boardSize);
+  if (starPoints !== undefined){
+    starPoints.stars.forEach(point => {
+      if (point[0] === gridX + 1 && point[1] === gridY + 1){
+        classNames.push(styles.star);
+      }
+    });
+  }
+  
   return classNames.join(' ');
 };
 
