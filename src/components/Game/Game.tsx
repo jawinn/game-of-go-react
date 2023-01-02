d61ee5539aa25f3550c734f05d1d8c48530c7cc1
Josh Winn <965114+jawinn@users.noreply.github.com>
2023-01-01 20:23:30 -0500

    feat: Working board size selection game phase
    Implement phases of the game that are rendered, starting with the intro
    board size selection screen, which then leads to gameplay and the
    rendered board.
    
    Working board selection grid that progresses to the next phase after a
    size is selected, and the start game button is pressed.

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,5 +1,5 @@
 import React, { useState, MouseEvent } from 'react';
-import './Game.css';
+import styles from './Game.module.css';
 import Board, { newBoardData } from 'components/Board/Board';
 import TurnIndicator from 'components/TurnIndicator/TurnIndicator';
 import { StoneType } from 'components/Stone/Stone';
@@ -18,10 +18,9 @@ export enum GamePhase {
  * Main Game (App)
  */
 function Game() {
-  // @todo Implement phases of game:
-  // const [gamePhase] = useState<GamePhase>(GamePhase.ChooseBoard);
+  const [gamePhase, setGamePhase] = useState<GamePhase>(GamePhase.ChooseBoard);
   const [turn, setTurn] = useState<boolean>(false);
-  const [boardSize] = useState<number>(9);
+  const [boardSize, setBoardSize] = useState<number>(0);
   const [boardData, setBoardData] = useState<StoneType[][]>(newBoardData(boardSize));
   const [players] = useState<IPlayer[]>(
     [
@@ -29,6 +28,12 @@ function Game() {
       newPlayer('Player 2')
     ]
   );
+
+  const startGame = (newBoardSize: number) => {
+    setBoardSize(newBoardSize);
+    setBoardData(newBoardData(newBoardSize));
+    setGamePhase(GamePhase.PlayingGame);
+  };
 
   /**
    * Place or remove a stone; change the StoneType at a single point on the board.
@@ -55,16 +60,77 @@ function Game() {
     setTurn(!turn);
   }
 
+  /**
+   * Render main game contents, based on current phase of game.
+   */
+  const renderGamePhase = (phase: GamePhase): React.ReactElement | null => {
+    switch (phase) {
+      case GamePhase.PlayingGame:
+        return (
+          <>
+            <TurnIndicator turn={turn} players={players} />
+            <Board 
+              boardSize={boardSize} 
+              boardData={boardData} 
+              handleClickPoint={handleClickPoint} 
+              turn={turn} />
+          </>
+        );
+
+      case GamePhase.ChooseBoard:
+      default:
+        return (
+          <>
+            <header className={styles.gameIntro}>
+              <h1 className={styles.title}>
+                <small>The Game of</small> Go
+              </h1>
+              <p className={styles.subtitle}>
+                Version: 0.1 &mdash; <em className={styles.status}>Not yet fully playable</em>
+              </p>
+            </header>
+
+            <article className={styles.content}>
+              <fieldset>
+                <legend>Choose a Board Size</legend>
+                <div className={styles.radioGrid}>
+                  <div className={styles.radioGridItem}>
+                    <input type="radio" value="9" id="boardSize-9" name="boardSize" onChange={(e) => setBoardSize(parseInt(e.target.value))} />
+                    <label htmlFor="boardSize-9">
+                      <div className={styles.itemHeading}>9 x 9</div>
+                      <div className={styles.itemSubheading}>Practice</div>
+                    </label>
+                  </div>
+                  <div className={styles.radioGridItem}>
+                    <input type="radio" value="13" id="boardSize-13" name="boardSize" onChange={(e) => setBoardSize(parseInt(e.target.value))} />
+                    <label htmlFor="boardSize-13">
+                      <div className={styles.itemHeading}>13 x 13</div>
+                      <div className={styles.itemSubheading}>Practice</div>
+                    </label>
+                  </div>
+                  <div className={styles.radioGridItem}>
+                    <input type="radio" value="19" id="boardSize-19" name="boardSize" onChange={(e) => setBoardSize(parseInt(e.target.value))} />
+                    <label htmlFor="boardSize-19">
+                      <div className={styles.itemHeading}>19 x 19</div>
+                      <div className={styles.itemSubheading}>Standard</div>
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+
+              <p>
+                <button type="button" disabled={!boardSize}>Start Game</button>
+              </p>
+            </article>
+          </>
+        );
+    }
+  }
+
   return (
-    <div className="game" data-testid="Game">
-      <TurnIndicator turn={turn} players={players} />
-      <Board 
-        boardSize={boardSize} 
-        boardData={boardData} 
-        handleClickPoint={handleClickPoint} 
-        turn={turn}
-      />
-    </div>
+    <main className={styles.game} data-testid="Game">
+      {renderGamePhase(gamePhase)}
+    </main>
   );
 }
 
